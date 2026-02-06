c7f01db05d2fa1637c6a60f9591832a441c0f164
Illia Dubina <armahanz.music@gmail.com>
2026-02-06 17:59:06 +0200

    chore: register models in admin and improve admin configuration

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    Worker,
+    TaskType,
+    Task,
+    Position,
+    Project,
+    Team,
+    Tag
+)
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "get_tags",
+        "priority",
+        "is_completed",
+        "deadline",
+        "task_type",
+        "project",
+    ]
+
+    @admin.display(description="Tags")
+    def get_tags(self, obj):
+        return ", ".join(f"#{tag.name}" for tag in obj.tags.all())
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+admin.site.register(Project)
+admin.site.register(Team)
+admin.site.register(Tag)
