10ea4b4e4d0165c4bf5619231e17d7fc57f9168c
Illia Dubina <armahanz.music@gmail.com>
2026-02-07 18:15:23 +0200

    feat: add index page
    What:
    - Add root route ("")
    - Add index function view with context data
    - Add index.html template with initial content

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from task_manager.views import index
 
 urlpatterns = [
-
+    path("", index, name="index"),
 ]
 
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import (
+    Task,
+    Project,
+    Worker,
+    Team,
+    Position
+)
+def index(request):
+
+    num_teams = Team.objects.count()
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+    num_projects = Project.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_teams": num_teams,
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_projects": num_projects,
+        "num_tasks": num_tasks,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
