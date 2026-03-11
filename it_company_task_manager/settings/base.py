3a7f7d8c8b3d5b53082ebead41a4bce4cd1cdc00
Illia Dubina <armahanz.music@gmail.com>
2026-03-11 15:37:20 +0200

    chore: add build.sh, install gunicorn

--- it_company_task_manager/settings/base.py
+++ it_company_task_manager/settings/base.py
@@ -22,7 +22,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-change-me")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default="django-insecure-change-me")
 
 # Application definition
 
